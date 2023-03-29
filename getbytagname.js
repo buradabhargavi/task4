6d8306693dbcde13888f3c89bb8b932687edf126
buradabhargavi <110055258+buradabhargavi@users.noreply.github.com>
2023-03-29 12:24:46 +0530

    fourthcommit

---
+++ getbytagname.js
@@ -0,0 +1,28 @@
+/* var items=document.getElementsByClassName('list-group-item');
+console.log("items are");
+console.log(items);
+console.log(items[1]);
+for(var i=0;i<items.length;i++){
+  items[i].style.fontWeight='bold';
+  // items[i].style.backgroundColor='gray';
+}
+//items[1].textContent='hello1';
+//items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='black';
+items[4].style.backgroundColor='green';  */
+
+
+
+
+ var li=document.getElementsByTagName('li');
+
+console.log(li);
+console.log(li[1]);
+for(var i=0;i<li.length;i++){
+  li[i].style.fontWeight='bold';
+  // items[i].style.backgroundColor='gray';
+}
+//items[1].textContent='hello1';
+//items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='black';
+li[4].style.backgroundColor='green'; 
